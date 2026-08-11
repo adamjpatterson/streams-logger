c0cb04db4bc209820272275563264b8b84fab0b5
Adam Patterson <adpatter2000@gmail.com>
2026-08-11 09:39:16 -0400

    Improve configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig([
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  { ignores: ["**/dist"] },
+  { ignores: ["**/dist/**", "**/node_modules/**"] },
   {
     languageOptions: {
       parserOptions: { project: ["./tsconfig.eslint.json"] },
@@ -23,9 +23,11 @@ export default defineConfig([
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx", "**/*.mjs", "**/*.cjs"],
     rules: {
+      "no-empty": ["off"],
       quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
       "@stylistic/semi": ["error", "always"],
       "@typescript-eslint/no-require-imports": ["off"],
+      "@typescript-eslint/no-empty-function": ["off"],
     },
   },
 ]);
